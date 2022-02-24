314ae71f9420f59330849a5ec6951d2ac277e2e1
Muraz <92208166+MurazGuliyev@users.noreply.github.com>
2022-02-24 19:12:22 +0400

    last muraz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Portfolio from './Porfolio';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import Footer from './Footer';
+import Footerinput from './FooterInput';
+import RiconConstraction from './RiconConstruction';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Salam Fuad <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/RiconConstraction" element={<RiconConstraction />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+function Layout() {
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="row">
+          <Portfolio />
 
+          <Footer />
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+}
 export default App;
---
+++ src/data.js
@@ -0,0 +1,97 @@
+import businessCard from './assets/img/business_card.png';
+import portfolioShape from './assets/img/portfolio_shape.svg';
+
+const Data = {
+  itemData: [
+    {
+      id: 1,
+      name_portfolio_1span: '2019 -1',
+      name_portfolio_2span: 'qısametrajlı film',
+      img: businessCard,
+      contentTitle: 'Ricony constraction',
+      contentp:
+        'Güclü bir marka, müştərilərin rəqiblərinizə nisbətən məhsul və yaxidmətlərinizdən yararlanma şansını artırır. Güclü bir marka,müştərilərin rəqiblərinizə nisbətən məhsul və ya xidmətlərinizdənyararlanma şansını artırır',
+      contentLinkSpan: 'Proyekt haqqında daha ətraflı',
+      contentLinkimg: portfolioShape,
+    },
+    {
+      id: 2,
+      name_portfolio_1span: '2019 -2',
+      name_portfolio_2span: 'qısametrajlı film',
+      img: businessCard,
+      contentTitle: 'Ricony constraction',
+      contentp:
+        'Güclü bir marka, müştərilərin rəqiblərinizə nisbətən məhsul və yaxidmətlərinizdən yararlanma şansını artırır. Güclü bir marka,müştərilərin rəqiblərinizə nisbətən məhsul və ya xidmətlərinizdənyararlanma şansını artırır',
+      contentLinkSpan: 'Proyekt haqqında daha ətraflı',
+      contentLinkimg: portfolioShape,
+    },
+    {
+      id: 3,
+      name_portfolio_1span: '2019 -3',
+      name_portfolio_2span: 'qısametrajlı film',
+      img: businessCard,
+      contentTitle: 'Ricony constraction',
+      contentp:
+        'Güclü bir marka, müştərilərin rəqiblərinizə nisbətən məhsul və yaxidmətlərinizdən yararlanma şansını artırır. Güclü bir marka,müştərilərin rəqiblərinizə nisbətən məhsul və ya xidmətlərinizdənyararlanma şansını artırır',
+      contentLinkSpan: 'Proyekt haqqında daha ətraflı',
+      contentLinkimg: portfolioShape,
+    },
+    {
+      id: 4,
+      name_portfolio_1span: '2019 -4',
+      name_portfolio_2span: 'qısametrajlı film',
+      img: businessCard,
+      contentTitle: 'Ricony constraction',
+      contentp:
+        'Güclü bir marka, müştərilərin rəqiblərinizə nisbətən məhsul və yaxidmətlərinizdən yararlanma şansını artırır. Güclü bir marka,müştərilərin rəqiblərinizə nisbətən məhsul və ya xidmətlərinizdənyararlanma şansını artırır',
+      contentLinkSpan: 'Proyekt haqqında daha ətraflı',
+      contentLinkimg: portfolioShape,
+    },
+    {
+      id: 5,
+      name_portfolio_1span: '2019 -5',
+      name_portfolio_2span: 'qısametrajlı film',
+      img: businessCard,
+      contentTitle: 'Ricony constraction',
+      contentp:
+        'Güclü bir marka, müştərilərin rəqiblərinizə nisbətən məhsul və yaxidmətlərinizdən yararlanma şansını artırır. Güclü bir marka,müştərilərin rəqiblərinizə nisbətən məhsul və ya xidmətlərinizdənyararlanma şansını artırır',
+      contentLinkSpan: 'Proyekt haqqında daha ətraflı',
+      contentLinkimg: portfolioShape,
+    },
+    {
+      id: 6,
+      name_portfolio_1span: '2019 -6',
+      name_portfolio_2span: 'qısametrajlı film',
+      img: businessCard,
+      contentTitle: 'Ricony constraction',
+      contentp:
+        'Güclü bir marka, müştərilərin rəqiblərinizə nisbətən məhsul və yaxidmətlərinizdən yararlanma şansını artırır. Güclü bir marka,müştərilərin rəqiblərinizə nisbətən məhsul və ya xidmətlərinizdənyararlanma şansını artırır',
+      contentLinkSpan: 'Proyekt haqqında daha ətraflı',
+      contentLinkimg: portfolioShape,
+    },
+    {
+      id: 7,
+      name_portfolio_1span: '2019 -7',
+      name_portfolio_2span: 'qısametrajlı film',
+      img: businessCard,
+      contentTitle: 'Ricony constraction',
+      contentp:
+        'Güclü bir marka, müştərilərin rəqiblərinizə nisbətən məhsul və yaxidmətlərinizdən yararlanma şansını artırır. Güclü bir marka,müştərilərin rəqiblərinizə nisbətən məhsul və ya xidmətlərinizdənyararlanma şansını artırır',
+      contentLinkSpan: 'Proyekt haqqında daha ətraflı',
+      contentLinkimg: portfolioShape,
+    },
+    {
+      id: 8,
+      name_portfolio_1span: '2019 -8',
+      name_portfolio_2span: 'qısametrajlı film',
+      img: businessCard,
+      contentTitle: 'Ricony constraction',
+      contentp:
+        'Güclü bir marka, müştərilərin rəqiblərinizə nisbətən məhsul və yaxidmətlərinizdən yararlanma şansını artırır. Güclü bir marka,müştərilərin rəqiblərinizə nisbətən məhsul və ya xidmətlərinizdənyararlanma şansını artırır',
+      contentLinkSpan: 'Proyekt haqqında daha ətraflı',
+      contentLinkimg: portfolioShape,
+    },
+  ],
+};
+
+export default Data;
